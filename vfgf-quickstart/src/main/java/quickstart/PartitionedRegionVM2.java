077db2be325fecd3a5fbf657682b9d61af761d44
David Roberts <ubiquitous_alias@yahoo.com>
2012-04-19 17:37:19 -0500

    added files for the Partitioned Data Caching Example Updated build to start apps

--- vfgf-quickstart/src/main/java/quickstart/PartitionedRegionVM2.java
+++ vfgf-quickstart/src/main/java/quickstart/PartitionedRegionVM2.java
@@ -3,8 +3,10 @@ package quickstart;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-import com.gemstone.gemfire.cache.Cache;
-import com.gemstone.gemfire.cache.CacheFactory;
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Component;
+
 import com.gemstone.gemfire.cache.Region;
 
 /**
@@ -16,27 +18,19 @@ import com.gemstone.gemfire.cache.Region;
  * 
  * @author GemStone Systems, Inc.
  */
-
+@Component
 public class PartitionedRegionVM2
 {
 
-  public static final String regionName = "PartitionedRegion";
+	// Represents region name for the partitioned region
+	// inject the region
+	@Resource(name = "PartitionedRegion")
+	private Region<String, String> pr;
 
-  public static void main(String[] args) throws Exception
+  public void execute() throws Exception
   {
 
-    System.out
-        .println("\nConnecting to the distributed system and creating the cache.");
 
-    // Create the cache which causes the cache-xml-file to be parsed
-    Cache cache = new CacheFactory()
-      .set("name", "PartitionedRegion")
-      .set("cache-xml-file", "xml/PartitionedRegion.xml")
-      .create();
-    System.out.println("\nExample region, " + regionName
-        + ", created in cache.");
-
-    Region<String,String> pr = cache.getRegion(regionName);
     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
     System.out.println("Please press Enter in VM1.");
@@ -44,7 +38,7 @@ public class PartitionedRegionVM2
 
     // Get three entries from the partitioned region
     final int endCount = 3;
-    System.out.println("Getting " + endCount + " entries from " + regionName  + ". . .");
+    System.out.println("Getting " + endCount + " entries from " + pr.getName()  + ". . .");
     for (int count = 0; count < endCount; count++) {
       String key = "key" + count;
       System.out.println("\n     Getting key " + key + ": " + pr.get(key));
@@ -54,7 +48,7 @@ public class PartitionedRegionVM2
     bufferedReader.readLine();
 
     // Get three entries from the partitioned region after destroy and invalidate
-    System.out.println("Getting the same entries from " + regionName
+    System.out.println("Getting the same entries from " + pr.getName()
         + " after destroy and invalidate. . .");
     for (int count = 0; count < endCount; count++) {
       String key = "key" + count;
@@ -62,8 +56,5 @@ public class PartitionedRegionVM2
     }
 
     System.out.println("\nAfter VM2 is closed, please press Enter in VM1 to read the values.");
-    // Close the cache and disconnect from GemFire distributed system
-    System.out.println("\nClosing the cache and disconnecting.");
-    cache.close();
   }
 }
